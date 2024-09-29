906972e609b6dd7ac22a071bbfcd4603756c3b29
Samuel Lima <105258182+Samuelrlm@users.noreply.github.com>
2024-09-29 03:42:36 -0300

    feat(compare):create router for compare filters

--- finance_manager/transactions/views.py
+++ finance_manager/transactions/views.py
@@ -3,7 +3,7 @@ import json
 from django.http import JsonResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from .controllers import TransactionController, TransactionDetailController
+from .controllers import TransactionController, TransactionDetailController, TransactionCompareController
 
 class TransactionFilterView(View):
     @csrf_exempt
@@ -33,3 +33,22 @@ class TransactionDetailView(View):
             return JsonResponse({"error": "Transação não encontrada."}, status=404)
 
         return JsonResponse(transaction)
+    
+class TransactionCompareView(View):
+    @csrf_exempt
+    def get(self, request):
+        controller = TransactionCompareController()
+
+        try:
+            body = json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Erro ao ler o corpo da requisição."}, status=400)
+
+        filters = body.get('filters', [])
+
+        if not filters:
+            return JsonResponse({"error": "Nenhum filtro informado."}, status=400)
+
+        filtered_transactions = controller.compare_transactions(filters)
+
+        return JsonResponse(filtered_transactions, safe=False)
